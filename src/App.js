844779cb5fe39e6666f53ba3931dcbe3be1e256c
gustavoeob <gustavoeliasoropezabaez@gmail.com>
2022-12-04 01:36:38 -0300

    spinner added

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import "./styles/scss/main.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Appbar from "./components/views/navbar/appbar";
 import Home from "./components/pages/home/home";
-import { ThemeContext } from "./context/ThemeContext";
-import { useContext } from "react";
+import { useEffect, useState } from "react";
+import Spinner from "./components/views/spinner";
 
 function App() {
-const {theme}  = useContext(ThemeContext);
+  useEffect(()=>{
+      setTimeout(()=> {
+        setLoading(false)
+      },2200)
+    },[])
+  const [loading, setLoading] = useState(true)
+  if (loading) return <Spinner />
+
   return (
-    <div id={theme}>
+    <div >
         <BrowserRouter>
           <Appbar />
           <Routes>
             <Route path="*" element={<Navigate to="/" />} />
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={!loading ? <Home /> : setLoading(true)} />
           </Routes>
         </BrowserRouter>
     </div>
---
+++ src/components/views/spinner.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./spinner.scss"
+
+const Spinner = () => {
+  return (
+    <>
+      <div className="spinner-container">
+        <div class="spinner"></div>
+      </div>
+    </>
+  );
+};
+
+export default Spinner;
